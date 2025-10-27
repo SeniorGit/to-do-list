8a0e74e680da02068af4993b6866f5247b58de80
Alfito Nur Fadhila <88030166+SeniorGit@users.noreply.github.com>
2025-10-27 18:24:27 +0700

    update image to webp

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ['to-do-list-rho-ashen.vercel.app'],
+    formats: ['image/webp', 'image/avif'], 
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
 };
 
 export default nextConfig;
