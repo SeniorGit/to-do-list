f0ad7d2d33f42180e8d4b8f52ce8f009171c7402
Alfito Nur Fadhila <88030166+SeniorGit@users.noreply.github.com>
2025-10-27 18:12:36 +0700

    Updating Error Deploymen on LadingPage Page.tsx

--- src/app/(landingPage)/landingPage/page.tsx
+++ src/app/(landingPage)/landingPage/page.tsx
@@ -1,18 +1,13 @@
 import CasesSection from "../component/casesSection";
 import FeatureSection from "../component/featureSection";
 import HeroSection from "../component/heroSection";
-import ShowCaseSection from "../component/showcaseSection";
-import SocialProofSection from "../component/sosialProofSection";
 
 export default function LandingPage() {
   return (
     <div>
       <HeroSection/>
       <FeatureSection/>
-      {/* showcase dan sosial butuh improment lebih lanjut */}
-      {/* <ShowCaseSection/>  */}
       <CasesSection/>
-      {/* <SocialProofSection/> */}
     </div>
   )
 }
